d95e3722473c3853e048db39e0d0d113c4788531
angelafafa <89323085+angelafafa@users.noreply.github.com>
2021-08-31 18:37:48 -0400

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,7 @@
+document.write('\
+<div class="hero-image">\
+<div class="hero-text">\
+    <h1 style="font-family: Lato;font-size:90px;font-weight: 700;">Yunhong Wu</h1>\
+  </div>\
+</div>\
+');
---
+++ navigation.js
@@ -0,0 +1,14 @@
+document.write('\
+\
+<div class="topnav">\
+<a href="about.html">About</a>\
+<a href="publication.html">Publications</a>\
+<a href="research.html">Research</a>\
+<a class="active" href="index1.html">Home</a>\
+<div class="leftop-text">\
+    <h1 style="font-family: Lato, sans-serif;\
+    font-size: 15pt;\
+    font-weight: 300;">∫(birth,death) study(t) dt=life</h1>\
+  </div>\
+</div>\
+');
